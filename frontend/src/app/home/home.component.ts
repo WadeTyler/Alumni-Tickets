0c3a613eb264af9eb361685a607441af4ded1052
Tyler Wade <vaxonreal@gmail.com>
2025-03-28 17:37:17 -0400

    Added Hero banner to home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {EventService} from '../core/services/event.service';
 import {EventType} from '../../../../types/event.types';
 import {EventPanelComponent} from '../shared/components/event-panel/event-panel.component';
 import {CreateEventPanelComponent} from '../shared/components/create-event-panel/create-event-panel.component';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {remixCalendarTodoLine} from '@ng-icons/remixicon';
 
 @Component({
   selector: 'app-home',
   imports: [
     EventPanelComponent,
-    CreateEventPanelComponent
+    CreateEventPanelComponent,
+    NgIcon
   ],
+  providers: [provideIcons({ remixCalendarTodoLine })],
   templateUrl: './home.component.html',
   styles: ``
 })
 export class HomeComponent {
 
   events: EventType[] = [];
+  @ViewChild('eventsContainer') eventsContainer!: ElementRef;
 
   constructor(protected eventService: EventService) { }
 
@@ -30,6 +35,10 @@ export class HomeComponent {
         console.error("Error loading events: ", error);
       }
     })
+  }
+
+  scrollToEventsContainer(): void {
+    this.eventsContainer.nativeElement.scrollIntoView({ behavior: "smooth" });
   }
 
 }
